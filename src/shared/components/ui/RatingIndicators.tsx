c3ace625a2f133614f5febdf80eb093981fb393e
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2024-11-17 18:22:01 -0800

    feat(RatingIndicators): add logic for rendering rating to nearest half

--- src/shared/components/ui/RatingIndicators.tsx
+++ src/shared/components/ui/RatingIndicators.tsx
@@ -2,17 +2,25 @@ import "./RatingIndicators.css";
 
 interface RatingIndicatorsProps {
   rating: number;
-};
+}
 
 function RatingIndicators({ rating }: RatingIndicatorsProps) {
-  // if (rating < 0) throw new Error("Rating must be a positive number"); // TODO revise test to accept Error?
   if (rating < 0 || rating > 5) return null;
 
   const indicators: React.ReactNode[] = [];
-  const description = `Rated ${rating} out of 5`;
+  const description = `Rated ${rating.toFixed(1)} out of 5`;
+
+  const roundedRating = Math.round(rating * 2) / 2;
+  const whole = Math.trunc(roundedRating);
 
   for (let i = 0; i < 5; i++) {
-    indicators.push(<span key={`rating-${i}`}>{i < rating ? "●" : "○"}</span>);
+    if (i === whole && roundedRating % 1 === 0.5) {
+      indicators.push(<span key={`rating-${i}`}>◐</span>);
+    } else {
+      indicators.push(
+        <span key={`rating-${i}`}>{i < roundedRating ? "●" : "○"}</span>
+      );
+    }
   }
 
   return (
