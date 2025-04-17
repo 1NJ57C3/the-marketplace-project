35df1631df9670bb4126da1a8d78f169d9ed4b64
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2025-04-16 19:26:03 -0700

    chore(users): add model with `is_seller` property

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+  is_seller = models.BooleanField(default=False)
+
+  def __str__(self):
+    self.username
