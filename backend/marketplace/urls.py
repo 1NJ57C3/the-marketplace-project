21baa90cc85df8ffcc022434964e865dbe061e3c
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2025-04-17 00:02:13 -0700

    docs(urls): update project docstring

--- backend/marketplace/urls.py
+++ backend/marketplace/urls.py
@@ -1,18 +1,7 @@
 """
-URL configuration for marketplace project.
+URL configuration for the Marketplace Django project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+Includes admin routes, DRF viewsets, and browsable API login/logout.
 """
 from django.urls import path, include
 from django.contrib import admin
