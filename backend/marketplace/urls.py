ce6ef4acaab8cd275fb82df0c64558337fff11fb
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2025-04-16 23:42:17 -0700

    chore(users): add DRF router, serializer, and viewset

--- backend/marketplace/urls.py
+++ backend/marketplace/urls.py
@@ -14,10 +14,27 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path, include
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+from rest_framework import routers, serializers, viewsets
+
+User = get_user_model()
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email', 'first_name', 'last_name', 'is_seller']
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth', include('restframework.urls'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api/', include(router.urls)),
 ]
