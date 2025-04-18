75b469d3da78b1d1878373992463a9be2c9cf67c
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2025-04-17 18:34:25 -0700

    feat(users): add registration serializer
    - Adds `UserCreateSerializer` with password confirmation and secure password handling

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -1,7 +1,29 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
+User = get_user_model()
+
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
-        model = get_user_model()
+        model = User
         fields = ['id', 'username', 'email', 'first_name', 'last_name', 'is_seller']
+        read_only_fields = ['id', 'is_seller']
+
+class UserCreateSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+    password2 = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'password2', 'first_name', 'last_name']
+        read_only_fields = ['is_seller']
+
+    def validate(self, data):
+        if data['password'] != data['password2']:
+            raise serializers.ValidationError("Passwords must match.")
+        return data
+
+    def create(self, validated_data):
+        validated_data.pop('password2')
+        user = User.objects.create_user(**validated_data)
+        return user
