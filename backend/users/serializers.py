9be55ef0528eed39b0a57eaca6d3f530cad8b5f8
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2025-04-18 14:09:19 -0700

    refactor(users): modularize auth routes and serializers

--- backend/users/serializers.py
+++ backend/users/serializers.py
@@ -8,22 +8,3 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         model = User
         fields = ['id', 'username', 'email', 'first_name', 'last_name', 'is_seller']
         read_only_fields = ['id', 'is_seller']
-
-class UserCreateSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
-    password2 = serializers.CharField(write_only=True)
-
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password', 'password2', 'first_name', 'last_name']
-        read_only_fields = ['is_seller']
-
-    def validate(self, data):
-        if data['password'] != data['password2']:
-            raise serializers.ValidationError("Passwords must match.")
-        return data
-
-    def create(self, validated_data):
-        validated_data.pop('password2')
-        user = User.objects.create_user(**validated_data)
-        return user
