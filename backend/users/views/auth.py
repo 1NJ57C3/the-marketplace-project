ce003f7a383733b0d06d7ffcbdd1a0b77ae77634
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2025-04-18 13:08:55 -0700

    feat(users): add logout view and route, reorganize routes

--- backend/users/views/auth.py
+++ backend/users/views/auth.py
@@ -1,5 +1,9 @@
 from django.contrib.auth import get_user_model
-from rest_framework import generics
+
+from rest_framework import generics, permissions, status
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
 
 from ..serializers import UserCreateSerializer
 
@@ -8,3 +12,15 @@ User = get_user_model()
 class UserRegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserCreateSerializer
+
+class LogoutView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
