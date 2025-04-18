2bbf177584fcbc5aab5e8ad0164beb44dc6a5f1d
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2025-04-17 23:43:18 -0700

    chore(settings): add JWT auth support via `simplejwt`

--- backend/marketplace/settings.py
+++ backend/marketplace/settings.py
@@ -105,6 +105,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # `djangorestframework` (DRF) config
 
 REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
