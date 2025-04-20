81f39103bb88a8833e7d1366b3f044f22882e7b8
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2025-04-19 22:03:24 -0700

    style(utils): format with `black`

--- backend/users/auth/utils.py
+++ backend/users/auth/utils.py
@@ -1,7 +1,8 @@
 from rest_framework import serializers
 
-def validate_password_confirmation(password: str, password2: str, field_name="password") -> None:
+
+def validate_password_confirmation(
+    password: str, password2: str, field_name="password"
+) -> None:
     if password != password2:
-        raise serializers.ValidationError({
-            field_name: "Passwords must match."
-        })
+        raise serializers.ValidationError({field_name: "Passwords must match."})
