1714ece1bfa7237f971aeaa16ac7a1f6ab8bba44
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2025-04-20 00:34:58 -0700

    refactor(auth): move `simplejwt` routes from core to auth urls

--- backend/marketplace/urls.py
+++ backend/marketplace/urls.py
@@ -5,16 +5,11 @@ Includes admin routes, browsable API auth, and registered DRF routes.
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/users/', include('users.urls')),
     path('api/auth/', include('users.auth.urls')),
 ]
