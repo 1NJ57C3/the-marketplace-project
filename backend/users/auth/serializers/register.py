90ddbdc508bd8c6264ad54aee1d864e6d751ede5
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2025-04-19 22:08:16 -0700

    refactor(register): change `password2` to `confirm_password`, format with `black`

--- backend/users/auth/serializers/register.py
+++ backend/users/auth/serializers/register.py
@@ -9,18 +9,25 @@ User = get_user_model()
 
 class UserCreateSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
-    password2 = serializers.CharField(write_only=True)
+    confirm_password = serializers.CharField(write_only=True)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password', 'password2', 'first_name', 'last_name']
-        read_only_fields = ['is_seller']
+        fields = [
+            "username",
+            "email",
+            "password",
+            "confirm_password",
+            "first_name",
+            "last_name",
+        ]
+        read_only_fields = ["is_seller"]
 
     def validate(self, data):
-        validate_password_confirmation(data['password'], data['password2'])
+        validate_password_confirmation(data["password"], data["confirm_password"])
         return data
 
     def create(self, validated_data):
-        validated_data.pop('password2')
+        validated_data.pop("confirm_password")
         user = User.objects.create_user(**validated_data)
         return user
