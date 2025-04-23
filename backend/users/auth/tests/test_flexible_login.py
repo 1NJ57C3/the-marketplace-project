43e603c396ef8bec7041bbed091f40b7b1b51695
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2025-04-22 19:06:53 -0700

    fix(tests): add missing `/`s to response URLs

--- backend/users/auth/tests/test_flexible_login.py
+++ backend/users/auth/tests/test_flexible_login.py
@@ -4,24 +4,24 @@ import pytest
 @pytest.mark.django_db
 def test_login_with_username(api_client, create_user):
     create_user()
-    response = api_client.post("api/auth/login", {
+    response = api_client.post("/api/auth/login/", {
         "username": "testuser",
         "password": "strongpassword123",
     }, format="json")
 
     assert response.status_code == 200
-    assert "access" in response.data or "token" in response.data
+    assert "access" in response.data
     assert "refresh" in response.data
 
 
 @pytest.mark.django_db
 def test_login_with_email(api_client, create_user):
     create_user()
-    response = api_client.post("api/auth/login", {
+    response = api_client.post("/api/auth/login/", {
         "username": "test@example.com",
-        "password": "strongpassword123"
+        "password": "strongpassword123",
     }, format="json")
 
     assert response.status_code == 200
-    assert "access" in response.data or "token" in response.data
+    assert "access" in response.data
     assert "refresh" in response.data
