60222ed1748c94416bf293e0c9131b27d6748574
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2025-04-22 21:57:40 -0700

    refactor(login): restructure response body, correct "id" key, nest token pair under "tokens"

--- backend/users/auth/serializers/login.py
+++ backend/users/auth/serializers/login.py
@@ -3,13 +3,14 @@ from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 
 class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
     def validate(self, attrs):
-        data = super().validate(attrs)
-        data.update({
-            "user.id": self.user.id,
+        tokens = super().validate(attrs)
+        user_data = {
+            "id": self.user.id,
             "username": self.user.username,
             "email": self.user.email,
             "first_name": self.user.first_name,
             "last_name": self.user.last_name,
             "is_seller": self.user.is_seller,
-        })
-        return data
+            "tokens": tokens,
+        }
+        return user_data
