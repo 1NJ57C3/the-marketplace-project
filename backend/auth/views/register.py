db0393f0436ec906772a966dd6b35346ef2446aa
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2025-04-23 16:52:38 -0700

    fix(register): add missing `permission_classes`

--- backend/auth/views/register.py
+++ backend/auth/views/register.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-from rest_framework import generics
+from rest_framework import generics, permissions
 
 from auth.serializers.register import UserCreateSerializer
 
@@ -10,3 +10,4 @@ User = get_user_model()
 class UserRegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserCreateSerializer
+    permission_classes = [permissions.AllowAny]
