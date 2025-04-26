59fda8c136c2aa4fe18b6cc2c19798b6afc661ff
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2025-04-26 02:34:58 -0700

    fix(test-password-reset): add missing view and route

--- backend/auth/urls.py
+++ backend/auth/urls.py
@@ -8,6 +8,7 @@ from auth.views.login import CustomLoginView as LoginView
 from auth.views.logout import LogoutView
 from auth.views.register import UserRegisterView
 from auth.views.password_reset_request import CustomPasswordResetRequestView as PasswordResetRequestView
+from auth.views.password_reset_confirm import CustomPasswordResetConfirmView as PasswordResetConfirmView
 
 
 urlpatterns = [
@@ -17,6 +18,7 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('change-password/', ChangePasswordView.as_view(), name='change-password'),
     path('password-reset/', PasswordResetRequestView.as_view(), name='password_reset'),
+    path('password-reset/confirm/', PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 ]
 
 __all__ = ['urlpatterns']
---
+++ backend/auth/views/password_reset_confirm.py
@@ -0,0 +1,9 @@
+from rest_framework.views import APIView
+# 1. instantiate form
+# 2. validate form
+# 3. fill form data
+# 4. save form
+# 5. return appropriate response
+
+class CustomPasswordResetConfirmView(APIView):
+    ''
