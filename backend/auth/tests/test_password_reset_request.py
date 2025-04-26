e7e01f5081c1b775d18ef24720e08b8ba4ab34d5
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2025-04-26 02:14:16 -0700

    test(password-reset): add endpoint test and helper

---
+++ backend/auth/tests/test_password_reset_request.py
@@ -0,0 +1,23 @@
+import pytest
+from django.core import mail
+
+
+from tests.utils import send_password_reset_email
+
+@pytest.mark.django_db
+def test_password_reset_request_sends_email(use_locmem_email_backend, api_client, create_user):
+    """
+    Given a user's email, request a reset and verify an email is sent.
+    """
+    # 1. Create user with known email and password
+    create_user()
+
+    # 2. Request password reset for that email
+    email = "test@example.com"
+    resp = send_password_reset_email(api_client, email)
+    assert resp.status_code == 200
+
+    # 3. Assert that the email was sent to the correct address
+    assert len(mail.outbox) == 1
+    assert email in mail.outbox[0].to
+    #.TODO something like `assert {found_token} in mail.outbox[0].body`
--- backend/tests/utils.py
+++ backend/tests/utils.py
@@ -1,11 +1,14 @@
-def login_user(api_client, username="testuser", password="strongpassword123"):
+from rest_framework.test import APIClient
+from django.core.handlers.wsgi import WSGIRequest
+
+def login_user(api_client: APIClient, username="testuser", password="strongpassword123"):
     return api_client.post("/api/auth/login/", {
         "username": username,
         "password": password,
     }, format="json")
 
 
-def authenticate_client(api_client, token):
+def authenticate_client(api_client: APIClient, token: str):
     """
     Sets the Authorization header on the given APIClient.
     """
@@ -13,13 +16,13 @@ def authenticate_client(api_client, token):
     return api_client
 
 
-def logout_user(api_client, refresh_token):
+def logout_user(api_client: APIClient, refresh_token: str):
     return api_client.post("/api/auth/logout/", {
         "refresh": refresh_token,
     }, format="json")
 
 
-def change_password(api_client, old_password="strongpassword123", new_password="strongerpassword123"):
+def change_password(api_client: APIClient, old_password="strongpassword123", new_password="strongerpassword123"):
     return api_client.post("/api/auth/change-password/", {
         "old_password": old_password,
         "new_password": new_password,
@@ -27,7 +30,7 @@ def change_password(api_client, old_password="strongpassword123", new_password="
     }, format="json")
 
 
-def get_tokens(response, token="pair"):
+def get_tokens(response: WSGIRequest, token="pair"):
     """
     Retrieve JWT token(s) from a login response.
 
@@ -51,3 +54,10 @@ def get_tokens(response, token="pair"):
     elif token == "refresh":
         return tokens["refresh"]
     raise ValueError('Token argument must be "access", "refresh", "pair"')
+
+
+def send_password_reset_email(api_client: APIClient, email="test@example.com"):
+    """Request a password reset email."""
+    return api_client.post("/api/auth/password-reset/", {
+        "email": email,
+    }, format="json")
