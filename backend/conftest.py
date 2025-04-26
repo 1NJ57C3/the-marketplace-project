117b4c36e8fe4c59466e9b2cc6be8dc209d3a089
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2025-04-26 02:29:03 -0700

    fix(testing): add `locmem` email backend override fixture

--- backend/conftest.py
+++ backend/conftest.py
@@ -1,9 +1,16 @@
 import pytest
 from django.contrib.auth import get_user_model
+from django.test import override_settings
 from rest_framework.test import APIClient
 
 
 User = get_user_model()
+
+
+@pytest.fixture()
+def use_locmem_email_backend():
+    with override_settings(EMAIL_BACKEND='django.core.mail.backends.locmem.EmailBackend'):
+        yield
 
 
 @pytest.fixture
